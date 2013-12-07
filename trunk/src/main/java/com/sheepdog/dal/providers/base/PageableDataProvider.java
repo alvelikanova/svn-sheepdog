43a109fc956ee837b0439e31d40c145c84fe2a13
velikanova.alenka@gmail.com <velikanova.alenka@gmail.com@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2013-12-07 09:50:37 +0000

    Changed PageableDataProvider's getObjects & added BaseDataProviderImpl

--- trunk/src/main/java/com/sheepdog/dal/providers/base/PageableDataProvider.java
+++ trunk/src/main/java/com/sheepdog/dal/providers/base/PageableDataProvider.java
@@ -3,11 +3,11 @@ package com.sheepdog.dal.providers.base;
 import com.sheepdog.dal.providers.pagination.LoadOptions;
 import com.sheepdog.dal.providers.pagination.PagedList;
 
-public interface PageableDataProvider<T> {
+public interface PageableDataProvider<K> {
 
 	/**
 	 * Gets {@link PagedList} of entries using given {@link LoadOptions}
 	 * 
 	 */
-	PagedList<T> getObjects(LoadOptions loadOptions);
+	public PagedList<K> getObjects(LoadOptions loadOptions, Class dalEntityClass, Class domainEntityClass);
 }
---
+++ trunk/src/main/java/com/sheepdog/dal/providers/impl/BaseDataProviderImpl.java
@@ -0,0 +1,59 @@
+package com.sheepdog.dal.providers.impl;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sheepdog.dal.providers.base.GenericDataProvider;
+import com.sheepdog.dal.providers.base.PageableDataProvider;
+import com.sheepdog.dal.providers.pagination.LoadOptions;
+import com.sheepdog.dal.providers.pagination.PagedList;
+
+public abstract class BaseDataProviderImpl<K, T, ID extends Serializable> implements GenericDataProvider<T,ID>, PageableDataProvider<K>{
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public PagedList<K> getObjects(LoadOptions loadOptions, Class dalEntityClass, Class domainEntityClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(T entity) {
+		Session session = sessionFactory.getCurrentSession();
+		session.saveOrUpdate(entity);
+	}
+
+	@Override
+	public void merge(T entity) {
+		Session session = sessionFactory.getCurrentSession();
+		session.merge(entity);
+	}
+
+	@Override
+	public void delete(T entity) {
+		Session session = sessionFactory.getCurrentSession();
+		session.delete(entity);
+	}
+
+	@Override
+	public T findById(Class clazz, Integer id) {
+		Session session = sessionFactory.getCurrentSession();
+		T t = (T)session.get(clazz, id);
+		return t;
+	}
+
+	@Override
+	public List<T> findAll(Class clazz) {
+		Session session = sessionFactory.getCurrentSession();
+		Query query = session.createQuery("from "+clazz.getSimpleName());
+		return (ArrayList<T>) query.list();
+	}
+}
