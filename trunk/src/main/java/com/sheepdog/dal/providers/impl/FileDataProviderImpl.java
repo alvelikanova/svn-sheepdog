3c7a0c4e0dd70695fd7125b1b53a8ab7732b0005
velikanova.alenka@gmail.com <velikanova.alenka@gmail.com@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2013-12-14 18:27:31 +0000

    Added implementations of data providers

---
+++ trunk/src/main/java/com/sheepdog/dal/providers/impl/FileDataProviderImpl.java
@@ -0,0 +1,32 @@
+package com.sheepdog.dal.providers.impl;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sheepdog.business.domain.entities.File;
+import com.sheepdog.dal.entities.FileEntity;
+import com.sheepdog.dal.providers.FileDataProvider;
+
+@Repository
+public class FileDataProviderImpl extends BaseDataProviderImpl<FileEntity, File, Integer> implements FileDataProvider{
+
+	@Transactional
+	@Override
+	public File findFileByQualifiedName(String qualifiedName) {
+		File file = null;
+		try{
+			Criteria cr = sessionFactory.getCurrentSession()
+					.createCriteria(FileEntity.class)
+					.add(Restrictions.eq("QUALIFIED_NAME", qualifiedName));
+			cr.setMaxResults(1);
+			FileEntity fileEntity = (FileEntity) cr.uniqueResult();
+			file = mappingService.map(fileEntity, File.class);
+		} catch (Exception ex){
+			LOG.error("Error loading file", ex.getMessage());
+		}
+		return file;
+	}
+
+}
---
+++ trunk/src/main/java/com/sheepdog/dal/providers/impl/ProjectDataProviderImpl.java
@@ -0,0 +1,49 @@
+package com.sheepdog.dal.providers.impl;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sheepdog.business.domain.entities.Project;
+import com.sheepdog.dal.entities.ProjectEntity;
+import com.sheepdog.dal.providers.ProjectDataProvider;
+
+@Repository
+public class ProjectDataProviderImpl extends BaseDataProviderImpl<ProjectEntity, Project, Integer> implements ProjectDataProvider{
+
+	@Transactional
+	@Override
+	public Project findProjectByUrl(String url) {
+		Project project = null;
+		try{
+			Criteria cr = sessionFactory.getCurrentSession()
+					.createCriteria(ProjectEntity.class)
+					.add(Restrictions.eq("URL", url));
+			cr.setMaxResults(1);
+			ProjectEntity projectEntity = (ProjectEntity) cr.uniqueResult();
+			project = mappingService.map(projectEntity, Project.class);	
+		} catch (Exception ex) {
+			LOG.error("Error loading project", ex.getMessage());
+		}
+		return project;
+	}
+
+	@Transactional
+	@Override
+	public Project findProjectByName(String projectName) {
+		Project project = null;
+		try{
+			Criteria cr = sessionFactory.getCurrentSession()
+					.createCriteria(ProjectEntity.class)
+					.add(Restrictions.eq("NAME", projectName));
+			cr.setMaxResults(1);
+			ProjectEntity projectEntity = (ProjectEntity) cr.uniqueResult();
+			project = mappingService.map(projectEntity, Project.class);	
+		} catch (Exception ex) {
+			LOG.error("Error loading project", ex.getMessage());
+		}
+		return project;
+	}
+
+}
