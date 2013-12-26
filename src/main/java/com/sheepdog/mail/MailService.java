9570f9a1a2775be2477c45be1ed80503bd7ead0e
ivan.spread@gmail.com <ivan.spread@gmail.com@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2013-12-26 09:44:19 +0000

    Was renamed MailConnector. Was modified MailService to load properties from file. Was added mail.properies file. Was separated templates. Was deleted depricated templates. Was added some docs.

--- src/main/java/com/sheepdog/mail/MailService.java
+++ src/main/java/com/sheepdog/mail/MailService.java
@@ -1,6 +1,10 @@
 package com.sheepdog.mail;
 
+import java.io.IOException;
 import java.util.Map;
+
+import javax.security.auth.RefreshFailedException;
+import javax.xml.transform.TransformerException;
 
 import com.sheepdog.business.domain.entities.Subscription;
 import com.sheepdog.business.domain.entities.Tweet;
@@ -20,8 +24,16 @@ public interface MailService {
 	 * 
 	 * @param necessarySubscriptions
 	 *            Containing User objects, files and type of their changes.
+	 * @throws IOException
+	 *             if the message cannot be sent.
+	 * @throws TransformerException
+	 *             if template merge is failed.
+	 * @throws RefreshFailedException
+	 *             if update process is failed. See log for more details.
+	 * 
 	 */
-	public void sendMailBySubscription(Map<Subscription, TypeOfFileChanges> necessarySubscriptions);
+	public void sendMailBySubscription(Map<Subscription, TypeOfFileChanges> necessarySubscriptions)
+			throws RefreshFailedException, IOException, TransformerException;
 
 	/**
 	 * Send mail, that containing info about comment to revision of required
@@ -31,7 +43,22 @@ public interface MailService {
 	 *            Tweet object containing revision info.
 	 * @param user
 	 *            Author of revision.
+	 * @throws IOException
+	 *             if the message cannot be sent.
+	 * @throws TransformerException
+	 *             if template merge is failed.
+	 * @throws RefreshFailedException
+	 *             if update process is failed. See log for more details.
 	 */
-	public void sendMailByTweet(Tweet tweet, User user);
+	public void sendMailByTweet(Tweet tweet, User user) throws RefreshFailedException, IOException,
+			TransformerException;
+
+	/**
+	 * Reload updated properties and reconfiguring mail service.
+	 * 
+	 * @throws RefreshFailedException
+	 *             if update process is failed. See log for more details.
+	 */
+	public void resetConfig() throws RefreshFailedException;
 
 }
--- src/main/java/com/sheepdog/mail/impl/MailServiceImpl.java
+++ src/main/java/com/sheepdog/mail/impl/MailServiceImpl.java
@@ -1,9 +1,14 @@
 package com.sheepdog.mail.impl;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.security.auth.RefreshFailedException;
+import javax.xml.transform.TransformerException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.sheepdog.business.domain.entities.Subscription;
 import com.sheepdog.business.domain.entities.Tweet;
@@ -11,49 +16,69 @@ import com.sheepdog.business.domain.entities.User;
 import com.sheepdog.business.services.svn.impl.TypeOfFileChanges;
 import com.sheepdog.mail.MailService;
 
+@Service
 public class MailServiceImpl implements MailService {
 
 	@Autowired
-	private MailConnection mailConnection;
+	private MailConnector mailConnector;
 
-	public MailServiceImpl(MailConnection mailConnection) {
+	public MailServiceImpl(MailConnector mailConnector) {
 		super();
-		this.mailConnection = mailConnection;
+		this.mailConnector = mailConnector;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sheepdog.mail.MailService#sendMailBySubscription(java.util.Map)
+	 */
 	@Override
-	public synchronized void sendMailBySubscription(Map<Subscription, TypeOfFileChanges> necessarySubscriptions) {
-		mailConnection.openConnection();
-		
+	public synchronized void sendMailBySubscription(Map<Subscription, TypeOfFileChanges> necessarySubscriptions)
+			throws RefreshFailedException, IOException, TransformerException {
+		mailConnector.openConnection();
+
 		Map<User, Map<Subscription, TypeOfFileChanges>> subscriptions = new HashMap<>(0);
 
-
-		for (Subscription s : necessarySubscriptions.keySet()) {
-			
-			if (!subscriptions.containsKey(s.getUser())){
-				subscriptions.put(s.getUser(), new HashMap<Subscription,TypeOfFileChanges>(0));
-				subscriptions.get(s.getUser()).put(s, necessarySubscriptions.get(s));
-				continue;
+		for (Map.Entry<Subscription, TypeOfFileChanges> entry : necessarySubscriptions.entrySet()) {
+			if (!subscriptions.containsKey(entry.getKey().getUser())) {
+				subscriptions.put(entry.getKey().getUser(), new HashMap<Subscription, TypeOfFileChanges>(0));
 			}
-
-			subscriptions.get(s.getUser()).put(s, necessarySubscriptions.get(s));
-			
+			subscriptions.get(entry.getKey().getUser()).put(entry.getKey(), entry.getValue());
 		}
 
-		for (User u : subscriptions.keySet()) {
-			mailConnection.send(u, subscriptions.get(u), null);
+		for (Map.Entry<User, Map<Subscription, TypeOfFileChanges>> entry : subscriptions.entrySet()) {
+			mailConnector.sendSuscriptionMessage(entry.getKey(), entry.getValue());
 		}
 
-		mailConnection.closeConnection();
+		mailConnector.closeConnection();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.sheepdog.mail.MailService#sendMailByTweet(com.sheepdog.business.domain
+	 * .entities.Tweet, com.sheepdog.business.domain.entities.User)
+	 */
 	@Override
-	public synchronized void sendMailByTweet(Tweet tweet, User user) {
-		mailConnection.openConnection();
-		 
-		mailConnection.send(user, new HashMap(0), tweet);
+	public synchronized void sendMailByTweet(Tweet tweet, User user) throws RefreshFailedException, IOException,
+			TransformerException {
+		mailConnector.openConnection();
 
-		mailConnection.closeConnection();
+		mailConnector.sendTweetMessage(user, tweet);
+
+		mailConnector.closeConnection();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sheepdog.mail.MailService#resetConfig()
+	 */
+	@Override
+	public synchronized void resetConfig() throws RefreshFailedException {
+		mailConnector.setup();
+
 	}
 
 }
