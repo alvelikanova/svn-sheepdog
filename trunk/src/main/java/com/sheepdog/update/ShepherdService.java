fa3e1ec12d7993a5d0baab3ab857ea26a206e80b
ivan.spread@gmail.com <ivan.spread@gmail.com@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2014-01-24 07:36:35 +0000

    Was completed update service.

--- trunk/src/main/java/com/sheepdog/update/ShepherdService.java
+++ trunk/src/main/java/com/sheepdog/update/ShepherdService.java
@@ -2,9 +2,20 @@ package com.sheepdog.update;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * ShepherdService check updates from repository, update DB and invoke methods
+ * of MailService to send notification emails.
+ * 
+ * @author Ivan Arkhipov
+ * 
+ */
 @Service
 public interface ShepherdService {
 
+	/**
+	 * Method look on repository changes and make required updates and
+	 * notifications.
+	 */
 	public void lookOn();
-	
+
 }
--- trunk/src/main/java/com/sheepdog/update/impl/ShepherdServiceImpl.java
+++ trunk/src/main/java/com/sheepdog/update/impl/ShepherdServiceImpl.java
@@ -30,6 +30,16 @@ import com.sheepdog.business.services.svn.impl.TypeOfFileChanges;
 import com.sheepdog.mail.MailService;
 import com.sheepdog.update.ShepherdService;
 
+/**
+ * Implementation of {@link ShepherdService}. First, service check repository
+ * changes. If repository was modified, service load and save changes
+ * information into DB. Then, if SVN Sheepdog user added some files, service
+ * create new subscriptions and check existing subscriptions. After that
+ * ShepherdServiceImpl invoke mail service's method to send notification emails.
+ * 
+ * @author Ivan Arkhipov
+ * 
+ */
 @Service
 public class ShepherdServiceImpl implements ShepherdService {
 
@@ -38,30 +48,62 @@ public class ShepherdServiceImpl implements ShepherdService {
 	 */
 	public static final Logger LOG = (Logger) LoggerFactory.getLogger(ShepherdServiceImpl.class);
 
+	/**
+	 * Load new revision info from repository.
+	 */
 	@Autowired
 	private SVNRevisionService svnRevisionService;
 
+	/**
+	 * Load new file info from repository.
+	 */
 	@Autowired
 	private SVNFileService svnFileService;
 
+	/**
+	 * Update and save new file's info into DB.
+	 */
 	@Autowired
 	private FileManagementService fileManagementService;
 
+	/**
+	 * RevisionManagementService save new revision info into DB and check latest
+	 * revision.
+	 */
 	@Autowired
 	private RevisionManagementService revisionManagementService;
 
+	/**
+	 * Save new subscriptions and check existing subscriptions.
+	 */
 	@Autowired
 	private SubscriptionManagementService subscriptionManagementService;
 
+	/**
+	 * Check authors of repository changes.
+	 */
 	@Autowired
 	private UserManagementService userManagementService;
 
+	/**
+	 * Send notification emails.
+	 */
 	@Autowired
 	private MailService mailService;
 
+	/**
+	 * Fresh information about repository changes.
+	 */
 	private Map<Revision, Map<File, TypeOfFileChanges>> newRevisionsAndFiles;
 
+	/**
+	 * Required UPDATE_USER.
+	 */
 	private final User updateUser = User.getUpdateUser();
+
+	public ShepherdServiceImpl() {
+
+	}
 
 	public ShepherdServiceImpl(SVNRevisionService svnRevisionService, SVNFileService svnFileService,
 			FileManagementService fileManagementService, RevisionManagementService revisionManagementService,
@@ -77,6 +119,11 @@ public class ShepherdServiceImpl implements ShepherdService {
 		this.newRevisionsAndFiles = newRevisionsAndFiles;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.sheepdog.update.ShepherdService#lookOn()
+	 */
 	@Override
 	public void lookOn() {
 		long currentRevision = 0;
@@ -102,6 +149,19 @@ public class ShepherdServiceImpl implements ShepherdService {
 		}
 	}
 
+	/**
+	 * Method load current revision from repository and compare it with latest
+	 * revision from DB.
+	 * 
+	 * @param currentRevision
+	 *            Latest revision from DB.
+	 * @return True - if number of latest repository revision is more than
+	 *         number of current revision in DB. False - if revisions is equals.
+	 * @throws RefreshFailedException
+	 *             if failed connection to repository or authentication is
+	 *             failed, or number of latest repository revision is less then
+	 *             number of current revision in DB.
+	 */
 	private boolean checkUpdates(long currentRevision) throws RefreshFailedException {
 
 		Revision latestRepositoryRevision = new Revision();
@@ -130,6 +190,15 @@ public class ShepherdServiceImpl implements ShepherdService {
 		return false;
 	}
 
+	/**
+	 * Load new revisions and files info from repository.
+	 * 
+	 * @param currentRevision
+	 *            Number of latest revision in DB.
+	 * @throws RefreshFailedException
+	 *             if failed connection to repository or authentication is
+	 *             failed
+	 */
 	private void loadRevisions(long currentRevision) throws RefreshFailedException {
 		newRevisionsAndFiles.clear();
 		Set<Revision> newRevisionsSet = new HashSet<>(0);
@@ -153,6 +222,15 @@ public class ShepherdServiceImpl implements ShepherdService {
 
 	}
 
+	/**
+	 * Method check changes info and, if SVN Sheepdog user added some files,
+	 * create new subscriptions and save it in DB. Also method check existing
+	 * subscriptions and invoke methods of mailservice to send notification
+	 * emails.
+	 * 
+	 * @throws RefreshFailedException
+	 *             if mail service is failed on sending emails.
+	 */
 	private void subscribeManagement() throws RefreshFailedException {
 		Map<Subscription, TypeOfFileChanges> newSubscriptions = new HashMap<Subscription, TypeOfFileChanges>(0);
 		Map<Subscription, TypeOfFileChanges> necessarySubscriptions = new HashMap<Subscription, TypeOfFileChanges>(0);
@@ -170,14 +248,14 @@ public class ShepherdServiceImpl implements ShepherdService {
 			}
 
 			for (Map.Entry<File, TypeOfFileChanges> entry : newRevisionsAndFiles.get(r).entrySet()) {
-				// tempSubscriptions =
-				// subscriptionManagementService.getSubscriptionsByFile(e); TODO
+
+				tempSubscriptions = subscriptionManagementService.getSubscriptionsByFile(entry.getKey());
+
 				if (!tempSubscriptions.isEmpty()) {
 					for (Subscription s : tempSubscriptions) {
 						necessarySubscriptions.put(s, entry.getValue());
 					}
-					// fileManagementService.updateFilesRevision(entry.getKey);
-					// TODO
+					fileManagementService.updateFilesRevision(entry.getKey());
 				}
 			}
 		}
@@ -186,8 +264,6 @@ public class ShepherdServiceImpl implements ShepherdService {
 
 		try {
 			mailService.sendMailBySubscription(necessarySubscriptions);
-		} catch (RefreshFailedException e) {
-			throw new RefreshFailedException();
 		} catch (IOException e) {
 			throw new RefreshFailedException();
 		} catch (TransformerException e) {
@@ -196,6 +272,16 @@ public class ShepherdServiceImpl implements ShepherdService {
 
 	}
 
+	/**
+	 * Method check map with changed files. If some files was added, create and
+	 * return new subscriptions.
+	 * 
+	 * @param tempFiles
+	 *            Changed files.
+	 * @param user
+	 *            User of SVN Sheepdog and author of revision.
+	 * @return New subscriptions.
+	 */
 	private Map<Subscription, TypeOfFileChanges> subscribeNewFiles(Map<File, TypeOfFileChanges> tempFiles, User user) {
 		Map<Subscription, TypeOfFileChanges> subscriptions = new HashMap<Subscription, TypeOfFileChanges>(0);
 
