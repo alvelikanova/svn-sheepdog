40a8025ac260434e5b193dc6e2743f28a05d2900
ivan.spread@gmail.com <ivan.spread@gmail.com@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2014-03-04 12:09:19 +0000

    Was added currentUser filed into LoginManager.java

--- src/main/java/com/sheepdog/frontend/beans/pages/LoginManager.java
+++ src/main/java/com/sheepdog/frontend/beans/pages/LoginManager.java
@@ -16,77 +16,93 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-@ManagedBean (name = "authenticationBean")
+import com.sheepdog.business.domain.entities.User;
+import com.sheepdog.business.services.UserManagementService;
+
+@Component(value = "authenticationBean")
+@Scope("session")
 public class LoginManager implements Serializable {
-	
+
 	private static final long serialVersionUID = 6003658343558553848L;
 	private static final Logger LOG = LoggerFactory.getLogger(LoginManager.class);
-    private String username;
-    private String password;
-    private boolean remember;
-    
+	private String username;
+	private String password;
+	private boolean remember;
+
+	@Autowired
+	private UserManagementService ums;
+
+	private User currentUser = null;
+
+	public User getCurrentUser() {
+		return currentUser;
+	}
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public boolean isRemember() {
 		return remember;
 	}
-	
+
 	public void setRemember(boolean remember) {
 		this.remember = remember;
 	}
-	
-    private void facesError(String message) {
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
-    }
-    
-    public void doLogin() {
-        Subject subject = SecurityUtils.getSubject();
 
-        UsernamePasswordToken token = new UsernamePasswordToken(getUsername(), getPassword(), isRemember());
+	private void facesError(String message) {
+		FacesContext.getCurrentInstance()
+				.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+	}
 
-        try {
-            subject.login(token);
-            ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-            ec.redirect(ec.getRequestContextPath());
-//            if (subject.hasRole("admin")) {
-//                FacesContext.getCurrentInstance().getExternalContext().redirect("admin/index.xhtml");
-//            }
-//            else {
-//                FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
-//            }
-        }
-        catch (UnknownAccountException ex) {
-            facesError("Unknown account");
-        }
-        catch (IncorrectCredentialsException ex) {
-            facesError("Wrong password");
-        }
-        catch (AuthenticationException e) {
-        	facesError("Authentication failed");
-        }
-        catch (IOException ex) {
-            LOG.error(ex.getMessage(), ex);
-        }
-        finally {
-            token.clear();
-        }
-    }
-    
+	public void doLogin() {
+		Subject subject = SecurityUtils.getSubject();
+
+		UsernamePasswordToken token = new UsernamePasswordToken(getUsername(), getPassword(), isRemember());
+
+		try {
+			subject.login(token);
+			ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+			ec.redirect(ec.getRequestContextPath());
+			// if (subject.hasRole("admin")) {
+			// FacesContext.getCurrentInstance().getExternalContext().redirect("admin/index.xhtml");
+			// }
+			// else {
+			// FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+			// }
+
+			currentUser = ums.getUserByLogin(getUsername());
+
+		} catch (UnknownAccountException ex) {
+			facesError("Unknown account");
+		} catch (IncorrectCredentialsException ex) {
+			facesError("Wrong password");
+		} catch (AuthenticationException e) {
+			facesError("Authentication failed");
+		} catch (IOException ex) {
+			LOG.error(ex.getMessage(), ex);
+		} finally {
+			token.clear();
+		}
+
+	}
+
 	public String doLogout() {
 		LOG.trace("doLogout()");
 		SecurityUtils.getSubject().logout();
