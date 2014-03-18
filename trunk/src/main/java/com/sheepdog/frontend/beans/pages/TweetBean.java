e87c01efcdea83017658484fe2a7ec6050c4acb6
ivan.spread@gmail.com <ivan.spread@gmail.com@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2014-03-18 12:57:35 +0000

    Was modified some pages

--- trunk/src/main/java/com/sheepdog/frontend/beans/pages/TweetBean.java
+++ trunk/src/main/java/com/sheepdog/frontend/beans/pages/TweetBean.java
@@ -1,23 +1,37 @@
 package com.sheepdog.frontend.beans.pages;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.security.auth.RefreshFailedException;
+import javax.xml.transform.TransformerException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.sheepdog.business.domain.entities.Revision;
 import com.sheepdog.business.domain.entities.Tweet;
+import com.sheepdog.business.domain.entities.User;
 import com.sheepdog.business.services.TweetManagementService;
+import com.sheepdog.business.services.UserManagementService;
 import com.sheepdog.frontend.beans.templates.FeedbackBean;
+import com.sheepdog.initializer.Initializer;
+import com.sheepdog.mail.MailService;
 
 @Component(value = "tweetBean")
 @Scope("session")
 public class TweetBean {
+
+	/**
+	 * Logger object.
+	 */
+	public static final Logger LOG = LoggerFactory.getLogger(TweetBean.class);
 
 	@Autowired
 	private TweetManagementService tms;
@@ -28,13 +42,19 @@ public class TweetBean {
 	@Autowired
 	private FeedbackBean feedback;
 
+	@Autowired
+	private MailService mailService;
+
+	@Autowired
+	private UserManagementService ums;
+
 	private String tweetMessage;
 
 	private List<Tweet> tweets;
 
 	public void loadTweets(Revision revision) {
 		List<Tweet> list = new ArrayList<>();
-		// tms.getTweetsByRevision(Revision revision); TODO
+		// tms.getTweetsByRevision(revision);
 
 		if (list == null) {
 			tweets = new ArrayList<Tweet>(0);
@@ -50,7 +70,21 @@ public class TweetBean {
 
 		tweetMessage = "";
 
-		// tms.saveTweet(tweet);
+		tms.saveTweet(tweet);
+
+		User user = ums.getUserByLogin(revision.getAuthor());// TODO EMAIL
+																// SERACH
+		user = ums.getUserByLogin("ivan.spread");// TODO
+
+		try {
+			mailService.sendMailByTweet(tweet, user);// TODO
+		} catch (RefreshFailedException e) {
+			LOG.warn("Failed to send tweet notification to user : " + user.getLogin());
+		} catch (IOException e) {
+			LOG.warn("Failed to send tweet notification to user : " + user.getLogin() + ". Connection problem.");
+		} catch (TransformerException e) {
+			LOG.warn("Failed to send tweet notification to user : " + user.getLogin() + ". Template merging is failed.");
+		}
 
 	}
 
