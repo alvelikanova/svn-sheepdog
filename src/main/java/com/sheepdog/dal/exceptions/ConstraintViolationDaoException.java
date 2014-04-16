061708764119015281caa5e6b98523e9687f6d2c
velikanova.alenka <velikanova.alenka@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2014-04-16 13:07:03 +0000

    Added log to usersbean, changed ConstraintViolationDaoException

--- src/main/java/com/sheepdog/dal/exceptions/ConstraintViolationDaoException.java
+++ src/main/java/com/sheepdog/dal/exceptions/ConstraintViolationDaoException.java
@@ -2,18 +2,23 @@ package com.sheepdog.dal.exceptions;
 
 public class ConstraintViolationDaoException extends DaoException {
 	private static final long serialVersionUID = 1380249345567233298L;
-	private String constraintField;
+	private String constraintField = null;
 	public ConstraintViolationDaoException(Throwable cause) {
 		super(cause);
-		constraintField = null;
 	}
 	public ConstraintViolationDaoException(String message, Throwable cause) {
 		super(message, cause);
-		constraintField = null;
 	}
 	public ConstraintViolationDaoException(String message, Throwable cause, String constraintName) {
 		super(message, cause);
-		constraintField = constraintName.substring(constraintName.lastIndexOf('_') + 1).toLowerCase();
+		if (constraintName != null) {
+			int index = constraintName.lastIndexOf('_');
+			if (index != -1) {
+				constraintField = constraintName.substring(constraintName.lastIndexOf('_') + 1).toLowerCase();
+			} else {
+				constraintField = constraintName;
+			}
+		}
 	}
 	public String getConstraintField() {
 		return constraintField;
