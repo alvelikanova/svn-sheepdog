75b32574c1d9659f3d960d057813bf493f118587
velikanova.alenka <velikanova.alenka@4e6774cd-dc1f-ac76-79d3-01fb14ddd1a2>
2014-04-18 13:43:28 +0000

    Was deleted DaoUtils, was modified profile page

--- src/main/java/com/sheepdog/dal/exceptions/ConstraintViolationDaoException.java
+++ src/main/java/com/sheepdog/dal/exceptions/ConstraintViolationDaoException.java
@@ -1,26 +1,14 @@
 package com.sheepdog.dal.exceptions;
 
 public class ConstraintViolationDaoException extends DaoException {
-	private static final long serialVersionUID = 1380249345567233298L;
-	private String constraintField = null;
+	
+	private static final long serialVersionUID = 6304771815180108689L;
+	
 	public ConstraintViolationDaoException(Throwable cause) {
 		super(cause);
 	}
+	
 	public ConstraintViolationDaoException(String message, Throwable cause) {
 		super(message, cause);
-	}
-	public ConstraintViolationDaoException(String message, Throwable cause, String constraintName) {
-		super(message, cause);
-		if (constraintName != null) {
-			int index = constraintName.lastIndexOf('_');
-			if (index != -1) {
-				constraintField = constraintName.substring(constraintName.lastIndexOf('_') + 1).toLowerCase();
-			} else {
-				constraintField = constraintName;
-			}
-		}
-	}
-	public String getConstraintField() {
-		return constraintField;
 	}
 }
